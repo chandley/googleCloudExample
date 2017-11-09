f2a2564ab9616cfe056b1d9e73fcd7a8a9f6697b
Chris Handley <chandley@users.noreply.github.com>
2017-11-09 14:49:56 +0000

    server several endpoints

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 
 func init() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "You requested %s/n", r.URL.Path)
+		fmt.Fprintf(w, "Sorry %s is the wrong answer", r.URL.Path)
+	})
+	http.HandleFunc("/question", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "What is 2 plus 2", r.URL.Path)
+	})
+	http.HandleFunc("/4", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "4 is correct", r.URL.Path)
 	})
 }
