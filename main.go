e2831757a10b1551909891a3aef722be7e31d2d7
Chris Handley <chandley@users.noreply.github.com>
2017-11-09 14:46:56 +0000

    change to init func

--- main.go
+++ main.go
@@ -5,9 +5,8 @@ import (
 	"fmt"
 )
 
-func Main() {
+func init() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "You requested %s/n", r.URL.Path)
 	})
-	http.ListenAndServe(":8080", nil)
 }
